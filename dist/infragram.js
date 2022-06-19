df0f0f8e258da540a8f949594ebeadd538a254cd
FORCHA <forchapearl1@gmail.com>
2022-06-19 18:28:22 +0100

    Update infragram.js

--- dist/infragram.js
+++ dist/infragram.js
@@ -1,4 +1,5 @@
- isVideo = false,isCamera=false; // Turns off camera feed
+ isVideo = false,isCamera=false; // Track video and camera feeds
+
  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
  function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
